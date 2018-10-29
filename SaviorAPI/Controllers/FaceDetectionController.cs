21ebf54ee0cfda2eac25b3e5e474aec8f7951aa4
CORP\singhp3 <prachi.singhal@dvn.cpm>
2018-10-29 23:43:06 +0530

    Face Detectioon APi Package added

---
+++ SaviorAPI/Controllers/FaceDetectionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaviorAPI.Controllers
+{
+    public class FaceDetectionController : Controller
+    {
+        // GET: FaceDetection
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
